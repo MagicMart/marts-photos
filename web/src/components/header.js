6d6b7676e355fb89d9479c1961f230f2eae26fe7
Martin Tudor <martcan27@gmail.com>
2021-02-27 16:31:57 +0000

    update header styles

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -4,24 +4,38 @@ import styled from 'styled-components'
 import Nav from './nav'
 
 const StyledHeader = styled.header`
-  margin-bottom: 3rem;
+  margin: 3rem auto 0;
   display: grid;
   grid-template-rows: auto 1fr;
-  background: var(--yellow);
+  grid-template-columns: 1fr;
   h1 {
-    font-size: 2rem;
-    text-align: center;
+    font-size: 2.5rem;
+    width: fit-content;
+    margin: 0 auto;
+  }
+  .surname {
+    letter-spacing: 0.15em;
+  }
+  @media (min-width: 600px) {
+    & {
+      margin: 3rem auto 3rem;
+      grid-template-rows: 1fr;
+      grid-template-columns: auto 1fr;
+    }
+    h1 {
+      font-size: 4rem;
+    }
   }
 `
 
 const Header = ({ siteTitle }) => (
   <StyledHeader>
-    <Nav />
-    <h1>
+    <h1 className="tilt mark">
       <span>Martin</span>
       <br />
-      <span>Tudor</span>
+      <span className="surname">Tudor</span>
     </h1>
+    <Nav />
   </StyledHeader>
 )
 
--- web/src/components/nav.js
+++ web/src/components/nav.js
@@ -3,20 +3,33 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 
 const NavStyles = styled.nav`
-  margin-bottom: 3rem;
-  text-align: center;
-  font-size: 1.5rem;
+  margin: 3rem auto 3rem;
+  /* text-align: center; */
+  font-size: 1.75rem;
   ul {
-    text-decoration: none;
+    list-style-type: none;
     margin: 0;
     padding: 0;
     display: grid;
     grid-template-rows: repeat(6, auto);
+    gap: 1rem;
+  }
+  @media (min-width: 600px) {
+    & {
+      font-size: 1.8rem;
+    }
+    ul {
+      grid-template-rows: 1fr;
+      grid-template-columns: repeat(5, auto);
+    }
+    button {
+      display: none;
+    }
   }
 `
 
 export default function Nav() {
-  const [menuIsOpen, setMenuOpen] = React.useState(false)
+  const [menuIsOpen, setMenuOpen] = React.useState(true)
   const toggleMenu = () => setMenuOpen((c) => !c)
   return (
     <NavStyles>
