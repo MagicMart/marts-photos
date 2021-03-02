e3c152ccbb63a566084a95222010517148ae8c90
Martin Tudor <martcan27@gmail.com>
2021-03-02 15:38:58 +0000

    update project styles

--- web/src/templates/Project.js
+++ web/src/templates/Project.js
@@ -3,37 +3,56 @@ import Img from 'gatsby-image'
 import React from 'react'
 import styled from 'styled-components'
 
+const ProjectStyles = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto;
+  grid-row-gap: 1rem;
+  justify-content: center;
+`
+
 const PhotoArrayStyles = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, 100px);
 `
 
 const SinglePhotoStyles = styled.div`
+  position: relative;
   display: grid;
   grid-template-rows: 100px;
   border: 1px solid green;
+  .layer {
+    position: absolute;
+    background-color: rgba(248, 247, 216, 0.7);
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 `
 
 const SelectedStyles = styled.div`
-  max-width: 300px;
-  height: 300px;
+  width: 300px;
+  @media (min-width: 600px) {
+    width: 400px;
+  }
 `
 
 export default function SingleProject({ data }) {
   const [selected, setSelected] = React.useState(data.project.photos[0])
   return (
-    <>
+    <ProjectStyles>
       <SelectedStyles>
         <Img fluid={selected.image.asset.fluid} />
       </SelectedStyles>
       <PhotoArrayStyles>
         {data.project.photos.map((photo) => (
-          <SinglePhotoStyles onClick={() => setSelected(photo)}>
+          <SinglePhotoStyles key={photo.id} onClick={() => setSelected(photo)}>
             <Img fluid={photo.image.asset.fluid} />
+            {photo.id !== selected.id && <div className="layer"></div>}
           </SinglePhotoStyles>
         ))}
       </PhotoArrayStyles>
-    </>
+    </ProjectStyles>
   )
 }
 
