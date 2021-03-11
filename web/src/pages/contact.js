89a8cc3643d59a3283c69e33dfbe6dc4ff257a94
Martin Tudor <martcan27@gmail.com>
2021-03-11 17:07:47 +0000

    style contact page

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import SEO from '../components/seo'
+import styled from 'styled-components'
+
+const FormStyles = styled.form`
+  margin-top: 2rem;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-row-gap: 2rem;
+  label {
+    display: grid;
+    grid-template-columns: minmax(300px, 1fr);
+  }
+`
 
 export default function Home() {
   return (
     <>
       <SEO title="Contact" />
-      <h1>Contact page</h1>
+      <h2 className="mark" style={{ color: 'var(--red)' }}>
+        Contact
+      </h2>
+      <FormStyles>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Message:
+          <textarea value="" />
+        </label>
+        <input type="submit" value="Submit" />
+      </FormStyles>
     </>
   )
 }
