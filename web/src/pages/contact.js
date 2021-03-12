178632742404f5ec006e1c6d0d0d26420a6e76b4
Martin Tudor <martcan27@gmail.com>
2021-03-12 14:53:46 +0000

    add useForm

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SEO from '../components/seo'
 import styled from 'styled-components'
+import useForm from '../utils/useForm'
 
 const FormStyles = styled.form`
   margin-top: 2rem;
@@ -14,24 +15,46 @@ const FormStyles = styled.form`
 `
 
 export default function Home() {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+    message: '',
+  })
+
+  function handleSubmit(event) {
+    alert(JSON.stringify(values, null, 4))
+    event.preventDefault()
+  }
   return (
     <>
       <SEO title="Contact" />
-      <h2 className="mark" style={{ color: 'var(--red)' }}>
-        Contact
-      </h2>
-      <FormStyles>
+      <h2 style={{ color: 'var(--red)' }}>Contact</h2>
+      <FormStyles onSubmit={handleSubmit}>
         <label>
           Name:
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={updateValue}
+          />
         </label>
         <label>
           Email:
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={updateValue}
+          />
         </label>
         <label>
           Message:
-          <textarea value="" />
+          <textarea
+            name="message"
+            value={values.message}
+            onChange={updateValue}
+          />
         </label>
         <input type="submit" value="Submit" />
       </FormStyles>
