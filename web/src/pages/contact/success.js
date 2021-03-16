750682109277a6f6457d171df07e8ac700e68de8
Martin Tudor <martcan27@gmail.com>
2021-03-16 15:03:54 +0000

    add seo to success page

--- web/src/pages/contact/success.js
+++ web/src/pages/contact/success.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import SEO from '../../components/seo'
 
 export default function Success() {
   return (
     <>
+      <SEO title="Success" />
       <h2>Success</h2>
       <p>Your message was sent!</p>
+      <img src="/logo.svg" alt="Martin Tudor's logo" />
     </>
   )
 }
